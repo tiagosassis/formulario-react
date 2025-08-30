eee8fc86e48ac97aff655d0c715b0d23d0e6123b
Tiago Silva de Assis <tiaguiassis@gmail.com>
2025-08-30 17:06:14 -0300

    Create Button.jsx

---
+++ relatorio/src/components/Button.jsx
@@ -0,0 +1,10 @@
+const Button = ({id, classNameDiv, classNameInput, classNameLabel, type = "text", label, ...rest}) => {
+    return (
+        <div className={classNameDiv}>
+            <input type={type} id={id} className={classNameInput} {...rest} />
+            <label htmlFor={id} className={classNameLabel}>{label}</label>
+        </div>
+    )
+}
+
+export default Button
