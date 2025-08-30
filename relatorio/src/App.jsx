6118ab39f3b78ae11f61799c68bd3de0b8e04ddc
Tiago Silva de Assis <tiaguiassis@gmail.com>
2025-08-30 17:06:19 -0300

    Update App.jsx

--- relatorio/src/App.jsx
+++ relatorio/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Main from "./components/Main";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div id="app-container">
+        <Main />
+    </div>
   )
 }
 
